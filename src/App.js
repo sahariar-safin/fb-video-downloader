1b4c0c98fb87503c4758c3090944677e3ff58dc0
Shafin The Developer <sahariar350@gmail.com>
2021-12-31 14:23:58 +0600

    Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+// import fbVideoDownloader from 'fb-video-downloader';
+import { useEffect } from 'react';
+const facebookGetLink = require('facebook-video-link');
 
 function App() {
+
+  // const video = () => fbVideoDownloader.getInfo("https://www.facebook.com/watch?v=1106669323403327")
+  //   .then((info) => {
+  //     console.log(info);
+  //   })
+  // useEffect(() => {
+  //   video();
+  // }, [])
+
+  const url = 'https://www.facebook.com/EpochTimesTrending/videos/310155606660409';
+
+  const videoInfo = (url) => {
+    facebookGetLink(url).then(response => {
+      console.log(response)
+    });
+  }
+
+  useEffect(() => {
+    videoInfo(url);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
